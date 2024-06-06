83a11a69eb0988c6cd4ec8e28ad9ed764e108937
Srikanta V Surasagar <v-ssurasagar@microsoft.com>
2024-06-06 23:15:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace robotconsole
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ExecuteCommandsFrom("C:\\Users\\v-ssurasagar\\source\\repos\\Robot\\robotconsole\\Input2.txt");
+
+            Console.ReadLine();
+        }
+        private static void ExecuteCommandsFrom(string path)
+        {
+            var toyRobot = new toyrobot();
+            var simulator = new Simulator(toyRobot);
+
+            using (var file = new StreamReader(path))
+            {
+                string command;
+                while ((command = file.ReadLine()) != null)
+                {
+                    Console.WriteLine("Executing command: " + command);
+                    simulator.Execute(command);
+                }
+            }
+        }
+    }
+}
---
+++ Simulator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace robotconsole
+{
+    public class Simulator
+    {
+        private static readonly Regex _placeCommand = new Regex(@"PLACE (\d+),(\d+),(\w+)");
+
+        private readonly toyrobot _toyRobot;
+
+        public Simulator(toyrobot toyRobot)
+        {
+            _toyRobot = toyRobot;
+        }
+
+        public void Execute(string command)
+        {
+            if (string.IsNullOrWhiteSpace(command))
+                return;
+
+            if (command == "MOVE") _toyRobot.Move();
+            if (command == "LEFT") _toyRobot.Left();
+            if (command == "RIGHT") _toyRobot.Right();
+            if (command == "REPORT") Console.WriteLine(_toyRobot.Report());
+
+            var match = _placeCommand.Match(command);
+            if (match.Success)
+            {
+                var xIsValid = int.TryParse(match.Groups[1].Value, out var x);
+                var yIsValid = int.TryParse(match.Groups[2].Value, out var y);
+                var direction = match.Groups[3].Value;
+                if (xIsValid && yIsValid)
+                {
+                    _toyRobot.Place(x, y, direction);
+                }
+            }
+        }
+    }
+}
